678fcc6d7e9f464b550d57f4b503f8066c9ec3d9
Alex <alexiv.04@mail.ru>
2023-06-08 14:10:28 +0700

    Добавьте файлы проекта.

---
+++ SystemPer/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPer
+{
+    public class Class1
+    {
+        public string Calc(int a, string number)
+        {
+            int i = Convert.ToInt32(number);
+            switch (a)
+            {
+                case 16:
+                    return Convert.ToString(i, 16).ToUpper();
+                case 8:
+                    return Convert.ToString(i, 8).ToUpper();
+                case 2:
+                    return Convert.ToString(i, 2).ToUpper();
+                default:
+                    return Convert.ToString("Данная система счисления не поддерживается").ToUpper();
+
+            }
+        }
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SystemPer;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+
+            Assert.AreEqual("7E7", c.Calc(16,"2023"));
+            Assert.AreEqual("3747", c.Calc(8, "2023"));
+            Assert.AreEqual("11111100111", c.Calc(2, "2023"));
+        }
+    }
+}
